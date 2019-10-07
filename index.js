8d785c6c06b13e95ad21aa108065a9eee4ae4cd0
Georgi Petrushev <figoto660@gmail.com>
2019-10-07 06:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerLeft() {
+function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
  
