6cf91a1779c1f3fe0ce20603358be06bc1b22df8
Georgi Petrushev <figoto660@gmail.com>
2019-10-07 06:14:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ document.addEventListener('keydown', function(e) {
     var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
  
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left - 2}px`
   }
 })
