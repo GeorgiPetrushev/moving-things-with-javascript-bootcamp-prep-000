99e636784ae9c09ed19cee75fcc40f95f6ebf1ca
Georgi Petrushev <figoto660@gmail.com>
2019-10-07 06:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ function moveDodgerLeft() {
 }
 
 
+function moveDodgerRight(){
+    var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`
+  }
+}
+
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
