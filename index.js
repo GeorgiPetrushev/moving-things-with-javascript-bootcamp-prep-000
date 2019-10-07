bb1fef1d55618f67eddccb1ab83d1689585bec10
Georgi Petrushev <figoto660@gmail.com>
2019-10-07 06:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger');
 
 dodger.style.backgroundColor = "#FF69B4"
-dodget.style.left = "180px";
+dodger.style.bottom = '100px'
